bddeb82ddfda10e911545b0dd8edb652cc1cefab
dillonmchenry <dillon.mchenry7@gmail.com>
2019-12-09 10:19:23 -0500

    started option 2

--- main.py
+++ main.py
@@ -1,24 +1,12 @@
-import csv,sys
+import csv,sys, math
 from sensor import Sensor
 from date import Date
 # List of sensors
 sensors = []
 
-# Square location range
-lat_min = 0.0
-lat_max = 70.0
-long_min = -40.0
-long_max = 100.0
 
-# Circular location range
-lat = 0.0
-long = 0.0
-radius = 0.0
-
-# date
-
-
-def single_sensor(id, start, end):
+# Option 1
+def single_sensor(id, start, end="none"):
     with open("data_10sensors_1year.csv", "rt") as data:
         reader = data.readlines()
         # Breaks up stupid ass csv
@@ -32,6 +20,23 @@ def single_sensor(id, start, end):
             real.clear()
 
 
+# Option 2
+def location_mean(start, end, lat_min, long_min, radius="none", lat_max="none", long_max="none"):
+    in_bounds = []
+    if radius:
+        for sensor in sensors:
+            if in_circle(sensor.get_loc(), [lat_min, long_min], radius):
+                in_bounds.append(sensor)
+    else:
+        for sensor in sensors:
+            if in_square(sensor.get_loc(), [lat_min, lat_max], [lat_max, long_max]):
+                in_bounds.append(sensor)
+
+    for sensor in in_bounds:
+        # Call single sensor over the timespan for each sensor
+        single_sensor(sensor.get_id(), start, end)
+
+
 def fixline(line):
     new = []
     for i in range(1, len(line), 2):
@@ -41,11 +46,18 @@ def fixline(line):
 
 
 # Tests if lat long is in range of given parameters
-def in_range(loc, area_type):
-    if lat_min <= loc[0] <= lat_max and long_min <= loc[1] <= long_max:
+def in_square(loc, min, max):
+    if min[0] <= loc[0] <= max[0] and min[1] <= loc[1] <= max[1]:
         return True
     else:
         return False
+
+
+def in_circle(loc, center, radius):
+    distance = math.sqrt((loc[0]-center[0])**2 + (loc[1]-center[1])**2)
+    if distance < radius:
+        return True
+    return False
 
 
 # Collects all sensors in list
@@ -69,9 +81,13 @@ while True:
         start = input("Enter the start date of data collection (yyyy-mm-dd):\n")
         end = input("Enter the end date of collection (yyyy-mm-dd) or 'none' to collect a stamp\n")
         start = Date(start)
-        if end != "none":
-            end = Date(end)
+        if end == "none":
+            print(single_sensor(id, start))
         print(single_sensor(id, start, end))
+
+    elif choice == 2:
+        print("YO")
+        # Implement User interaction here
 
     elif choice == 5:
         print("Thank you for choosing AQMS...")
