a09febe72c56af0722bad4d232d976bef4b5db52
moboa <mbajifope@gmail.com>
2019-12-17 10:45:59 +0100

    Create date tests

--- tests/test_date.py
+++ tests/test_date.py
@@ -1,10 +1,17 @@
 from ..date import Date
 
+d1 = Date("2019-01-01")
+d2 = Date("2019-05-01")
+
 def test_in_span():
-    first = Date("2019-01-01")
-    second = Date("2019-05-01")
+    first = d1
+    second = d2
     third = Date("2019-10-01")
 
     assert second.in_span(first, third)
     assert not first.in_span(second, third)
     assert not third.in_span(second, first)
+
+def test_equal():
+    assert d1.equal(d1)
+    assert not d2.equal(d1)
